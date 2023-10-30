430990d35f527db1fbf7c6495a88e5c371da8efb
GodMaster17 <148480200+GodMaster17@users.noreply.github.com>
2023-10-30 14:15:35 -0400

    Update Player.js

--- Objects/Player.js
+++ Objects/Player.js
@@ -4,7 +4,6 @@ class Player extends Object {
         this.slowdownFactor = 1.25
     }
     update(){
-        var ccc = document.getElementById("SCREEN")
         if (this.yVel < 30) {
             this.yVel += gravity/smoothingFactor;
         } else {
@@ -12,26 +11,26 @@ class Player extends Object {
         }
         if (dPressed == true) {
             if (this.xVel < 75) {
-                this.xVel += 12
+                this.xVel += 6
             }
         }
         if (aPressed == true) {
             if (this.xVel > -75) {
-                this.xVel -= 12
+                this.xVel -= 6
             }
         }
         if (wPressed == true) {
-            if (this.y >= ccc.height-this.hitboxY) {
-            this.yVel = -45
+            if (this.y >= screenHeight-190) {
+            this.yVel = -45*0.75
             }
         }
         this.xVel /= (1 + (0.5/smoothingFactor)); //slowdown factor
-        if (this.y > ccc.height-this.hitboxY) {
-            this.y = ccc.height-this.hitboxY
+        if (this.y > screenHeight-190) {
+            this.y = screenHeight-190
             this.yVel = 0;
         }
-        if (this.x > ccc.width-this.hitboxX) {
-            this.x = ccc.width-this.hitboxX
+        if (this.x > screenWidth-this.width) {
+            this.x = screenWidth-this.width
         }
         if (this.x < 0) {
             this.x = 0
